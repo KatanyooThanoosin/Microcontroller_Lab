b599c07ed8a61040bc12bdb1cd008ab56ab6aa62
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2023-08-21 15:55:12 +0700

    Update Lab04_2.c

--- Lab04_2.c
+++ Lab04_2.c
@@ -3,9 +3,9 @@ char ch1 = '.' ;
 /* USER CODE END PV */
 
 /* USER CODE BEGIN 3 */
-	  		  HAL_UART_Transmit(&huart3, (uint8_t*) &ch1, 1, 1000);
-	  		  HAL_Delay(400);
-  /* USER CODE END 3 */
+HAL_UART_Transmit(&huart3, (uint8_t*) &ch1, 1, 1000);
+HAL_Delay(400);
+/* USER CODE END 3 */
 
 /* USER CODE BEGIN 4 */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
