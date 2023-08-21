5a76949e949800eb7a28cfa6ffcd4d126d4c835d
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2023-08-21 15:54:55 +0700

    Create Lab04_2.c

---
+++ Lab04_2.c
@@ -0,0 +1,23 @@
+/* USER CODE BEGIN PV */
+char ch1 = '.' ;
+/* USER CODE END PV */
+
+/* USER CODE BEGIN 3 */
+	  		  HAL_UART_Transmit(&huart3, (uint8_t*) &ch1, 1, 1000);
+	  		  HAL_Delay(400);
+  /* USER CODE END 3 */
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	if(GPIO_Pin == GPIO_PIN_0)
+	{
+		HAL_Delay(250);
+		HAL_GPIO_WritePin(GPIOB ,GPIO_PIN_0,!HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0));
+		HAL_UART_Transmit(&huart3, (uint8_t *) "---", 3,100);
+		for(int i=0; i<20;i++){
+			HAL_UART_Transmit(&huart3, (uint8_t *) "E", 1,100);
+			HAL_Delay(200);
+		}
+	}
+}
+/* USER CODE END 4 */
